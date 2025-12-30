deac4150cf0117e4f9213a688bb817b5bcdf43c2
Yaya <yayakari200@gmail.com>
2025-12-30 14:50:45 +0100

    feat: Creation des controllers (User , Auteur , Livre

---
+++ src/Controller/LivreController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class LivreController extends AbstractController
+{
+    #[Route('/livre', name: 'app_livre')]
+    public function index(): Response
+    {
+        return $this->render('livre/index.html.twig', [
+            'controller_name' => 'LivreController',
+        ]);
+    }
+}
